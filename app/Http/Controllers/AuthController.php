d2db9c82bde42392f5846c66e3280e7c63e5e698
= <=>
2021-07-14 22:57:07 +0100

    Alterei a forma de chamar o controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     public function dashboard(){
-        return ('admin.dashboard');
+        return view('admin.dashboard');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/admin','AuthController@dashboard')->name('admin');
+Route::get('/admin',[App\Http\Controllers\AuthController::class, 'dashboard'])->name('admin');
